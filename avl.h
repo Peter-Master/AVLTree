5594b808214b601b8169b53273a5d0a8a912dc5a
Peter Master <--global>
2016-11-26 20:22:23 -0800

    add relevant files

---
+++ avl.h
@@ -0,0 +1,32 @@
+#ifndef AVL_H_
+#define AVL_H_
+#define Element int32_t
+
+#include <string>
+
+struct BinaryNode {
+	Element elem;
+	BinaryNode *left;
+	BinaryNode *right;
+
+	BinaryNode(): elem(0), left(NULL), right(NULL) {}
+	BinaryNode(const Element & e, BinaryNode *lt, BinaryNode *rt): elem(e), left(lt), right(rt) {}
+};
+
+class AVLTree
+{
+public:
+  AVLTree ();
+  virtual ~AVLTree ();
+  void Insert (Element val);
+  // choose smallest node on the right subtree when deleting a node with two children
+  void Delete (Element val);
+  bool Access (Element val);
+  std::string PrintPreOrder () const;
+  std::string PrintInOrder () const;
+
+private:
+  /* data */
+};
+
+#endif /* end of include guard: AVL_H_ */
