253ec8e2b208acd22a30ca6071ac041f7d965630
Peter Master <--global>
2016-11-27 00:00:59 -0800

    add three partial private member functions

--- avl.cc
+++ avl.cc
@@ -19,7 +19,7 @@ void AVLTree::Delete(Element val) {
     Delete(val, root);
 }
 
-bool AVLTRee::Access(Element val) const {
+bool AVLTree::Access(Element val) const {
     return = Access(val, root);
 }
 
@@ -42,3 +42,52 @@ BinaryNode* AVLTree::getMin(BinaryNode *t) const {
     return root;
 }
 // SEE ABOVE MESSAGE
+
+void AVLTree::Insert(const Element e, BinaryNode * & t) {
+    if (t == NULL)
+        t = new BinaryNode(e);
+    else if (e > t->elem) {
+        Insert(e, t->right);
+    }
+    else if (e < t->elem) {
+        Insert(e, t->left);
+    }
+    else  //  e == t->elem
+        return;
+    // ***ROTATE***
+}
+
+void AVLTree::Delete(const Element e, BinaryNode * & t) {
+    if (t == NULL)
+        return false;
+    else if (e > t->elem)
+        Delete(e, t->right);
+    else if (e < t->elem)
+        Delete(e, t->left);
+    else { //  e == t->elem
+        BinaryNode *temp;
+        if (t->left && t->right) { //  t has two children
+            temp = getMin(t->right);
+            t->elem = temp->elem;
+        }
+        else //  t has zero or one children
+            t = t->left ? t->left : t->right;
+        delete temp;
+
+    }
+    // ***ROTATE*** once or twice
+    return;
+}
+
+bool AVLTree::Access(const Element e, BinaryNode * & t) const {
+    if (t == NULL)
+        return false;
+    else if (e > t->elem) {
+        Access(e, t->right);
+    }
+    else if (e < t->elem) {
+        Access(e, t->left);
+    }
+    else { //  (e == t->elem)
+        return true;
+}
