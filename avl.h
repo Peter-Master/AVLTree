6cbbbcf6e7f5730442a28734088f349ae15f4d74
Peter Master <--global>
2016-11-26 21:53:27 -0800

    add self as author to header file

--- avl.h
+++ avl.h
@@ -1,3 +1,6 @@
+// implements an AVL Tree
+// by Peter Master, 11/26/16
+
 #ifndef AVL_H_
 #define AVL_H_
 #define Element int32_t
@@ -10,23 +13,24 @@ struct BinaryNode {
 	BinaryNode *right;
 
 	BinaryNode(): elem(0), left(NULL), right(NULL) {}
-	BinaryNode(const Element & e, BinaryNode *lt, BinaryNode *rt): elem(e), left(lt), right(rt) {}
+	BinaryNode(const Element &e, BinaryNode *lt, BinaryNode *rt): elem(e), left(lt), right(rt) {}
 };
 
 class AVLTree
 {
 public:
-  AVLTree ();
-  virtual ~AVLTree ();
-  void Insert (Element val);
-  // choose smallest node on the right subtree when deleting a node with two children
-  void Delete (Element val);
-  bool Access (Element val);
-  std::string PrintPreOrder () const;
-  std::string PrintInOrder () const;
+    AVLTree ();
+    virtual ~AVLTree ();
+    void Insert (Element val);
+    // choose smallest node on the right subtree when deleting a node with two children
+    void Delete (Element val);
+    bool Access (Element val);
+    std::string PrintPreOrder () const;
+    std::string PrintInOrder () const;
 
 private:
-  /* data */
+
+    BinaryNode *root;
 };
 
 #endif /* end of include guard: AVL_H_ */
