705a25809dfcb55b22ae768abe483e6a5eb0aacd
Peter Master <--global>
2016-11-26 22:17:10 -0800

    format and add private data and member functions

--- avl.h
+++ avl.h
@@ -7,30 +7,33 @@
 
 #include <string>
 
-struct BinaryNode {
-	Element elem;
-	BinaryNode *left;
-	BinaryNode *right;
-
-	BinaryNode(): elem(0), left(NULL), right(NULL) {}
-	BinaryNode(const Element &e, BinaryNode *lt, BinaryNode *rt): elem(e), left(lt), right(rt) {}
-};
-
 class AVLTree
 {
 public:
     AVLTree ();
     virtual ~AVLTree ();
     void Insert (Element val);
-    // choose smallest node on the right subtree when deleting a node with two children
+    // chooses smallest node on the right subtree when deleting a node with two children
     void Delete (Element val);
     bool Access (Element val);
     std::string PrintPreOrder () const;
     std::string PrintInOrder () const;
 
 private:
+    struct BinaryNode {
+        Element elem;
+        BinaryNode *left;
+        BinaryNode *right;
 
-    BinaryNode *root;
+        BinaryNode(): elem(0), left(NULL), right(NULL) {}
+        BinaryNode(const Element &e, BinaryNode *lt, BinaryNode *rt): elem(e), left(lt), right(rt) {}
+    } *root;
+
+    BinaryNode* getMin(BinaryNode *t);
+    void makeEmpty(BinaryNode *t);
+	bool Insert(const Element e, BinaryNode * & t);
+	bool Access(const Element e, BinaryNode * & t);
+	bool Delete(const Element e, BinaryNode * & t);
 };
 
 #endif /* end of include guard: AVL_H_ */
