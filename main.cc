8a13ca6971cd160775d25badc5c85d787cf4b948
Peter Master <--global>
2016-11-30 20:56:58 -0800

    add some tests for the Access function in main

--- main.cc
+++ main.cc
@@ -13,6 +13,10 @@ TEST (AVLTree, Test1) {
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "20");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "20");
 
+
+  //for (int i = 0; i < 23; i++)
+  //  if (!avltree.COUNTER[i]) cout << i << " is false" << endl;
+
   avltree.Insert(10);
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "20 10");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "10 20");
@@ -31,7 +35,7 @@ TEST (AVLTree, Test1) {
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 10 15 18 20");
 
   avltree.Insert(12);
-  avltree.Access(15);
+  EXPECT_STREQ (avltree.Access(15) ? "t" : "f", "t");
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "15 10 2 12 18 20");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 10 12 15 18 20");
 
@@ -40,9 +44,11 @@ TEST (AVLTree, Test1) {
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 12 15 18 20");
   
   avltree.Delete(18);
+  EXPECT_STREQ (avltree.Access(18) ? "t" : "f", "f");
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "15 12 2 20");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 12 15 20");
 
+  EXPECT_STREQ (avltree.Access(10) ? "t" : "f", "f");
   avltree.Delete(20);
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "12 2 15");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 12 15");
@@ -51,6 +57,10 @@ TEST (AVLTree, Test1) {
   EXPECT_STREQ (avltree.PrintPreOrder().c_str(), "12 2 7 15");
   EXPECT_STREQ (avltree.PrintInOrder().c_str(), "2 7 12 15");
 
+  //for (int i = 0; i < 23; i++)
+  //  if (!avltree.COUNTER[i]) cout << i << " is false" << endl;
+
+  // the below has tested correct
   ifstream ifile;
   ofstream ofile;
   int N = 10000;
@@ -61,6 +71,8 @@ TEST (AVLTree, Test1) {
   for (int i = 0; i < N; i++) {
     ifile >> next;
     avltree2.Insert(next);
+    if (i == 5000)
+      avltree2.Insert(3501);
   }
   ifile.close();
   ifile.open("s10000.txt");
@@ -68,12 +80,23 @@ TEST (AVLTree, Test1) {
     ifile >> next;
     if (i % 3 == 0)
       avltree2.Delete(next);
+    if (i == 5000) {
+      avltree2.Delete(3501);
+      avltree2.Delete(50);
+      avltree2.Delete(550);
+      avltree2.Delete(341);
+    }
   }
   ofile << avltree2.PrintInOrder();
   ofile.close();
   ofile.open("s10000preorder.txt");
   ofile << avltree2.PrintPreOrder();
   ofile.close();
+
+  //cout << "anything false in final test?" << endl  << "if no following blanks then everything is true\n{" << endl;
+  for (int i = 0; i < 23; i++)
+    if (!avltree2.COUNTER[i]) cout << i << " is false" << endl;
+  //cout << "  }\n";
 }
 
 int main(int argc, char *argv[])
